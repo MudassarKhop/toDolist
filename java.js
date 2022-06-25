cf33836fd9355bdf96a94d7a1e041a1679591d6a
Mudassar <mudassarkhopatkar@gmail.com>
2022-06-25 23:15:22 +0200

    version 2

--- java.js
+++ java.js
@@ -1,7 +1,5 @@
-// On app load, get all tasks from localStorage
 window.onload = loadTasks;
 
-// On form submit add task
 document.querySelector("form").addEventListener("submit", (e) => {
 	e.preventDefault();
 	addTask();
@@ -17,7 +15,6 @@ function loadTasks() {
     }
 ];
 
-	// Loop through the tasks and add them to the list
 	tasks.forEach((task) => {
 		const list = document.querySelector("ul");
 		const li = document.createElement("li");
@@ -35,18 +32,14 @@ function loadTasks() {
 function addTask() {
 	const task = document.querySelector("form input");
 	const list = document.querySelector("ul");
-	// return if task is empty
 	if (task.value === "") {
 		alert("Please add some task!");
 		return false;
 	}
-	// check is task already exist
 	if (document.querySelector(`input[value="${task.value}"]`)) {
 		alert("Task already exist!");
 		return false;
 	}
-
-	// add task to local storage
 	localStorage.setItem(
 		"tasks",
 		JSON.stringify([
@@ -54,14 +47,11 @@ function addTask() {
 			{ task: task.value, completed: false },
 		])
 	);
-
-	// create list item, add innerHTML and append to ul
 	const li = document.createElement("li");
 	li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
       <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
       <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
 	list.insertBefore(li, list.children[0]);
-	// clear input
 	task.value = "";
 }
 
@@ -80,7 +70,6 @@ function removeTask(event) {
 	let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
 	tasks.forEach((task) => {
 		if (task.task === event.parentNode.children[1].value) {
-			// delete task
 			tasks.splice(tasks.indexOf(task), 1);
 		}
 	});
@@ -88,15 +77,12 @@ function removeTask(event) {
 	event.parentElement.remove();
 }
 
-// store current task to track changes
 let currentTask = null;
 
-// get current task
 function getCurrentTask(event) {
 	currentTask = event.value;
 }
 
-// edit the task and update local storage
 function editTask(event) {
 	let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
 	// check if task is empty
@@ -105,7 +91,6 @@ function editTask(event) {
 		event.value = currentTask;
 		return;
 	}
-	// task already exist
 	tasks.forEach((task) => {
 		if (task.task === event.value) {
 			alert("Task already exist!");
@@ -113,13 +98,11 @@ function editTask(event) {
 			return;
 		}
 	});
-	// update task
 	tasks.forEach((task) => {
 		if (task.task === currentTask) {
 			task.task = event.value;
 		}
 	});
-	// update local storage
 	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
